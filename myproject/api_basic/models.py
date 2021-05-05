a1aeefe97265ce4f8117f3cb8fb842649d62f08c
Miguel Figueroa <miguel@macheight.com>
2021-05-05 14:10:28 -0500

    Have built the barebones API with a GET and POST

--- myproject/api_basic/models.py
+++ myproject/api_basic/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class NBAplayers(models.Model):
 	first_name = models.CharField(max_length=100)
-	height_inches = models.DecimalField(max_digits=5, decimal_places=2)
-	height_meters = models.DecimalField(max_digits=5, decimal_places=2)
+	h_inches = models.DecimalField(max_digits=5, decimal_places=2)
+	h_meters = models.DecimalField(max_digits=5, decimal_places=2)
 	last_name = models.CharField(max_length=120)
 
 	def __str__(self):
--- myproject/api_basic/views.py
+++ myproject/api_basic/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
+from .models import NBAplayers
+from .serializers import NBAplayersSerializer
 
+
+def players_list(request):
+	if request.method == 'GET':
+		players = NBAplayers.objects.all()
+		serializer = NBAplayersSerializer(players, many=True)
+		return JsonResponse(serializer.data, safe=False)
+
+	elif request.method == 'POST':
+		data = JSONParser().parse(request)
+		serializer = NBAplayersSerializer(data=data)
+
+		if serializer.isvalid():
+			serializer.save()
+			return JsonResponse(serializer.data, status = 201)
+
+		return JsonResponse(serializer.errors, status=400)
 # Create your views here.
