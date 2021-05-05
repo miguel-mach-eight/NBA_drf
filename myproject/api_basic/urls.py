9b68440b723245c50c04f97b62ea44a4669b98b2
Miguel Figueroa <miguel@macheight.com>
2021-05-05 15:21:42 -0500

    Added PUT and DELETE with function-based views

--- myproject/api_basic/urls.py
+++ myproject/api_basic/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import players_list
+from .views import players_list, player_detail
 
 urlpatterns = [
     path('players/', players_list),
+    path('detail/<int:pk>/', player_detail)
 
 ]
--- myproject/api_basic/views.py
+++ myproject/api_basic/views.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
 from .models import NBAplayers
 from .serializers import NBAplayersSerializer
+from django.views.decorators.csrf import csrf_exempt
 
-
+@csrf_exempt
 def players_list(request):
 	if request.method == 'GET':
 		players = NBAplayers.objects.all()
@@ -21,3 +22,26 @@ def players_list(request):
 
 		return JsonResponse(serializer.errors, status=400)
 # Create your views here.
+@csrf_exempt
+def player_detail(request,pk):
+	try:
+		player = NBAplayers.objects.get(pk=pk)
+
+	except NBAplayers.DoesNotExist:
+		return HttpResponse(status=404)
+
+	if request.method == 'GET':
+		serializer = NBAplayersSerializer(player)
+		return JsonResponse(serializer.data)
+
+	elif request.method == 'PUT':
+		data = JSONParser().parse(request)
+		serializer = NBAplayersSerializer(player, data=data)
+		if serializer.isvalid():
+			serializer.save()
+			return JsonResponse(serializer.data)
+		return JsonResponse(serializer.errors, status=400)
+
+	elif request.method == 'DELETE':
+		player.delete()
+		return HttpResponse(status = 204)
