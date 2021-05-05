3cbbf8cfce92a0e5790517c277340ecfde45982b
Miguel Figueroa <miguel@macheight.com>
2021-05-05 13:19:45 -0500

    added a first serializer

--- myproject/api_basic/admin.py
+++ myproject/api_basic/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import NBAplayers
+
 
 # Register your models here.
+admin.site.register(NBAplayers)
---
+++ myproject/api_basic/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import NBAplayers
+
+
+class NBAplayersSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = NBAplayers 
+		fields = ['first_name','h_inches','h_meters', 'last_name']
+ 
