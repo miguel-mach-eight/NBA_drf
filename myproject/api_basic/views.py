7e598c6ddfb6ea610bd8091542329de6a98f3eae
Miguel Figueroa <miguel@macheight.com>
2021-05-06 17:14:30 -0500

    Replaced ViewSet implementation with ModelViewSet

--- myproject/api_basic/views.py
+++ myproject/api_basic/views.py
@@ -7,41 +7,9 @@ from rest_framework.response import Response
 from rest_framework import status, viewsets, permissions
 
 
-class NBAPlayersViewSet(viewsets.ViewSet):
-	#First evaluate whether the user is logged in or not. 
-	#If it's logged in, he can read/write.
-	# If it's not, he can only read.
-	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-	def list(self, request):
-		players = NBAplayers.objects.all()
-		serializer = NBAplayersSerializer(players, many=True)
-		return Response(serializer.data)
+class NBAPlayersViewSet(viewsets.ModelViewSet):
+	serializer_class = NBAplayersSerializer
+	queryset = NBAplayers.objects.all()
 
-	def create(self, request): #GET request to upload data to server
-		serializer = NBAplayersSerializer(data=request.data)
-
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status = status.HTTP_201_CREATED)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)	
-		#POST
-	def retrieve(self, request, pk=None): #POST request to receive data on end-point
-		query_set = NBAplayers.objects.all()
-		player = get_object_or_404(query_set, pk=pk)
-		serializer = NBAplayersSerializer(player)
-		return Response(serializer.data)
-		#PUT 
-	def update(self, request, pk=None): #PUT request to change data at the end point
-		player = NBAplayers.objects.get(pk=pk)
-		serializer = NBAplayersSerializer(player, data=request.	data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-		#DELETE
-	def destroy(self, request, pk=None): #DELETE request to erase an entry
-		player = NBAplayers.objects.get(pk=pk)
-		player.delete()
-		return Response(status = status.HTTP_204_NO_CONTENT)
-
+	
 
